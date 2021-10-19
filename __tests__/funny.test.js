0d64e4a8d0480ca623e31d2dc73158eed34bba84
Triana Cerda <80484840+trianacerda@users.noreply.github.com>
2021-10-18 17:48:11 -0700

    Revert 5 tdd for funny model (#6)
    * dev branch available. (#2)
    
    * Hotfix for sql table favorite (#4)
    
    * dev branch available.
    
    * setup tables and courses
    
    * seeding tables
    
    * failing test for /get random funny
    
    * added controller route to app.js for funnys
    
    * added SQL table favorite + building /get route for funny commits
    
    Co-authored-by: Simon Kaine <simonkaine@gmail.com>
    Co-authored-by: Simon kaine <67212475+simonkaine@users.noreply.github.com>
    Co-authored-by: Aemilius Morgan <aemiliusmorgan@gmail.com>
    
    * Tdd for funny model (#5)
    
    * dev branch available.
    
    * setup tables and courses
    
    * seeding tables
    
    * failing test for /get random funny
    
    * added controller route to app.js for funnys
    
    Co-authored-by: Simon Kaine <simonkaine@gmail.com>
    Co-authored-by: Simon kaine <67212475+simonkaine@users.noreply.github.com>
    Co-authored-by: Aemilius Morgan <aemiliusmorgan@gmail.com>
    
    * Revert "Tdd for funny model (#5)"
    
    This reverts commit e7d380b56358440770eff2a3953536839e5c3a7c.
    
    Co-authored-by: Simon kaine <67212475+simonkaine@users.noreply.github.com>
    Co-authored-by: Simon Kaine <simonkaine@gmail.com>
    Co-authored-by: Aemilius Morgan <aemiliusmorgan@gmail.com>

---
+++ __tests__/funny.test.js
@@ -0,0 +1,27 @@
+const pool = require('../lib/utils/pool.js');
+const setup = require('../data/setup.js');
+const request = require('supertest');
+const app = require('../lib/app.js');
+
+describe('slack-bot routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('should get a random funny entree with /GET', async () => {
+    return await request(app)
+      .get('/api/v1/funnys')
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          entree: expect.any(String),
+          course_id: expect.any(String),
+          times_viewed: expect.any(Number),
+        });
+      });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
