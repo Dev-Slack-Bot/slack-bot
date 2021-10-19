1fd747b19370d20be4f03aa6caca149348ec2ee0
Triana Cerda <trianaRcerda@gmail.com>
2021-10-19 15:32:39 -0700

    passing test for /post to user

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+const newUser = require('../lib/utils/user-utils');
 
 describe('slack-bot routes', () => {
   beforeEach(() => {
@@ -11,6 +12,7 @@ describe('slack-bot routes', () => {
   it('should post a user', async () => {
     return await request(app)
       .post('/api/v1/users')
+      .send(newUser)
       .then((res) => {
         expect(res.body).toEqual({
           id: expect.any(String),
