cd8c403747e5df737e4dba2aaf16b0d1c5b28d06
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-10-19 14:15:23 -0700

    passing get random tip

--- __tests__/tips.test.js
+++ __tests__/tips.test.js
@@ -1,22 +1,21 @@
 const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
-const app = require('../lib/app.js');
+const app = require('../lib/app');
 
 describe('slack-bot tip routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('should get a random tip with /GET', async () => {
+  it.only('should get a random tip with /GET', async () => {
     return await request(app)
       .get('/api/v1/tips')
       .then((res) => {
         expect(res.body).toEqual({
-          id: expect.any(String),
           tip: expect.any(String),
           tipUrl: expect.any(String),
-          courseId: expect.any(String),
+          course: expect.any(String),
           timesViewed: expect.any(Number),
         });
       });
