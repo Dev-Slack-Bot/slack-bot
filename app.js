73933ad527b9f8f622c834df463b81c14047301d
Triana Cerda <80484840+trianacerda@users.noreply.github.com>
2021-10-22 10:43:20 -0700

    Testing http routes on localhost (#35)
    * added all data from favorites, tips, funnys to test-- ALL TEST PASSING
    
    * working get by userId route with tests passing + rendering in 7890 and bolt

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ require('dotenv').config();
 const fetch = require('node-fetch');
 const Funny = require('./lib/models/Funny');
 
-
-
 // async function getUtil(bodyId) {
 //   const res = await fetch(`${process.env.BACKEND_URL}/favorites/${bodyId}`);
 //   const results = await res.json();
@@ -58,44 +56,43 @@ const choice = {
 // ----------------------------------------------- Initializes Favs bot/list
 app.message('favorites', async ({ say }) => {
   await say({
-    'blocks': [
+    blocks: [
       {
-        'type': 'section',
-        'text': {
-          'type': 'mrkdwn',
-          'text': 'Would you like to see your favorites list?'
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: 'Would you like to see your favorites list?',
         },
-        'accessory': {
-          'type': 'static_select',
-          'placeholder': {
-            'type': 'plain_text',
-            'text': 'Yes/No',
-            'emoji': true
+        accessory: {
+          type: 'static_select',
+          placeholder: {
+            type: 'plain_text',
+            text: 'Yes/No',
+            emoji: true,
           },
-          'options': [
+          options: [
             {
-              'text': {
-                'type': 'plain_text',
-                'text': 'Yes',
-                'emoji': true
+              text: {
+                type: 'plain_text',
+                text: 'Yes',
+                emoji: true,
               },
-              'value': 'seeFavs'
+              value: 'seeFavs',
             },
             {
-              'text': {
-                'type': 'plain_text',
-                'text': 'No',
-                'emoji': true
+              text: {
+                type: 'plain_text',
+                text: 'No',
+                emoji: true,
               },
-              'value': 'dismissFavs'
-            }
+              value: 'dismissFavs',
+            },
           ],
-          'action_id': 'static_select-action'
-        }
-      }
-    ]
-  }
-  );
+          action_id: 'static_select-action',
+        },
+      },
+    ],
+  });
 });
 
 // ----------------------------------------------- Initializes Slack bot
@@ -166,103 +163,102 @@ app.action('button_click', async ({ body, ack, say }) => {
     });
     // ------------------------------------nested action event for favorite choice
     app.action('static_select-action', async ({ ack, body, say }) => {
-      
       await ack();
       const randomFunnyJoke = await Funny.getData();
-      // console.log(' RANDO FUNNY ', randomFunnyJoke.id);
+      console.log(' RANDO FUNNY ', randomFunnyJoke.id);
       const favoritedValue = body.actions[0].selected_option.value;
       const bodyId = body.user.id;
       const userName = body.user.username;
       const name = body.user.name;
 
-
       // run our post route which will also check to see if user is in db
       // that post route should return a user whether its created or already exists
       // POST FAVS with user
-      if(favoritedValue === 'yes') {
-
-        await fetch(`${process.env.BACKEND_URL}/users`, { 
+      if (favoritedValue === 'yes') {
+        await fetch(`${process.env.BACKEND_URL}/users`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
-            Accept: 'application/json'
-          }, 
+            Accept: 'application/json',
+          },
           body: JSON.stringify({
-            id: bodyId, username: userName, name
-          })
+            id: bodyId,
+            username: userName,
+            name,
+          }),
         });
-  
-        const postFav = await fetch(`${process.env.BACKEND_URL}/favorites`, { 
+
+        const postFav = await fetch(`${process.env.BACKEND_URL}/favorites`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
-            Accept: 'application/json'
-          }, 
+            Accept: 'application/json',
+          },
           body: JSON.stringify({
-            userId: bodyId, 
-            tipsId: null, 
-            funnyId: `${randomFunnyJoke.id}`
-
-          })
-        }); 
+            userId: bodyId,
+            // tipsId: null,
+            funnyId: `${randomFunnyJoke.id}`,
+          }),
+        });
         console.log('FAV POST', postFav);
         await say(choice);
-
       }
-      
+
       // console.log(body.user.id, 'BODY USER ID FOR FAVS SELECT OPTION'); //U02JKAVFF96
 
       if (favoritedValue === 'seeFavs') {
         // need to access an array of favorited jokes now based off that getUtil function,
-        // then loop thru each favorited item. 
+        // then loop thru each favorited item.
         // const savedFavJoke = await fetch(`${process.env.BACKEND_URL}/favorites/${bodyId}`);
-        
+
         // ----------------------- USE THIS ONE BELOW INSTEAD
-        // const res = await fetch(`${process.env.BACKEND_URL}/favorites/${bodyId}`);
-        // return await res.json();
+        const res = await fetch(
+          `${process.env.BACKEND_URL}/favorites/${bodyId}`
+        );
+        const favoritesByUserId = await res.json();
+        console.log('RES JSON?!?!', favoritesByUserId);
+        return favoritesByUserId;
 
-        const res = await fetch(`${process.env.BACKEND_URL}/favorites/${bodyId}`, {
-          method: 'GET',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-        });
-        
-        console.log('RES JSON?!?!', await res.json());
-        return await res.json();
+        // const res = await fetch(`${process.env.BACKEND_URL}/favorites/${bodyId}`, {
+        //   method: 'GET',
+        //   headers: {
+        //     Accept: 'application/json',
+        //     'Content-Type': 'application/json',
+        //   },
+        // });
+
+        //   return await res.json();
       }
-    
-
-        
-        // await array1.forEach(favId => say({
-        //   'blocks': [
-        //     {
-        //       'type': 'section',
-        //       'text': {
-        //         'type': 'mrkdwn',
-        //         'text': 'Click to delete from your favs list !'
-        //       },
-        //       'accessory': {
-        //         'type': 'radio_buttons',
-        //         'options': [
-        //           {
-        //             'text': {
-        //               'type': 'plain_text',
-        //               'text': `${favId}`, // req.body.fav?
-        //               'emoji': true
-        //             },
-        //             'value': `${favId}` // if radio button value = favId, DELETE.
-        //           }
-        //         ],
-        //         'action_id': 'radio_buttons-action'
-        //       }
-        //     }
-        //   ]
-        // }));
-      
+      //   let array1 = [];
+      //   await array1.forEach((randomFunnyJoke) =>
+      //     say({
+      //       blocks: [
+      //         {
+      //           type: 'section',
+      //           text: {
+      //             type: 'mrkdwn',
+      //             text: 'Click to delete from your favs list !',
+      //           },
+      //           accessory: {
+      //             type: 'radio_buttons',
+      //             options: [
+      //               {
+      //                 text: {
+      //                   type: 'plain_text',
+      //                   text: `${randomFunnyJoke.entree}`, // req.body.fav?
+      //                   emoji: true,
+      //                 },
+      //                 value: `${randomFunnyJoke.entree}`, // if radio button value = favId, DELETE.
+      //               },
+      //             ],
+      //             action_id: 'radio_buttons-action',
+      //           },
+      //         },
+      //       ],
+      //     })
+      //   );
     });
-      
+
     // Return to this code block once all FUNNY stuff has worked - transfer over salvageable code from above.
   } else if (tipOrFunnyValue === 'tip') {
     await say({
@@ -289,9 +285,8 @@ app.action('button_click', async ({ body, ack, say }) => {
   }
 });
 
-
 (async () => {
   await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!'); 
+  console.log('⚡️ Bolt app is running!');
 })();
